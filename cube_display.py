fdd27b450977ea2f7a9ed2852d1ce25423b54bd7
Sanchit2809 <97045170+Sanchit2809@users.noreply.github.com>
2022-11-22 22:46:00 +0530

    files updated
    made the different files into single file and added the solver files

--- cube_display.py
+++
@@ -1,8 +0,0 @@
-from cube_structure import *
-def show(cube):
-    spacing = f'{" " * (len(str(cube.struct[0][0])) + 2)}'
-    l1 = '\n'.join(spacing + str(c) for c in cube.struct[0])
-    l2 = '\n'.join('  '.join(str(cube.struct[i][j]) for i in range(1,5)) for j in range(len(cube.struct[0])))
-    l3 = '\n'.join(spacing + str(c) for c in cube.struct[5])
-    print(f'{l1}\n\n{l2}\n\n{l3}')
-    
